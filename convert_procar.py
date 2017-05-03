e5c650b0f250686212c160372bb28e97c1a28b3d
Paulo V C Medeiros <pvm20@cam.ac.uk>
2017-05-03 13:06:31 +0100

    Removed convert_procar.py and get_orbital_dependent_EBS.py tools. They are no longer needed, since these steps have been incorporated into the python interface

--- convert_procar.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python
-from bandupy.vasp import procar2bandup
-
-#selected_ion_indices = xrange(32) # Only C
-#selected_ion_indices = range(32, 32+64) # Only Pt
-#selected_ion_indices = range(32, 32+8) # Only O
-procar2bandup()
