64856f9944522d5ed291477d6aa6609df8170025
Paulo V C Medeiros <pvm20@cam.ac.uk>
2017-05-17 14:51:00 +0100

    VASP tutorial: Example 2 adapted to new BandUP scheme

--- tutorial/VASP/example_2_bulk_Si/step_3_get_SC_wavefunctions_to_be_used_for_unfolding/job_for_band_unfolding.sh
+++ tutorial/VASP/example_2_bulk_Si/step_3_get_SC_wavefunctions_to_be_used_for_unfolding/job_for_band_unfolding.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-#SBATCH --nodes 24
-#SBATCH -A liu5 -p green
-#SBATCH -J  SiBulkBands
-#SBATCH -t 1-00:00:00
-# Path to VASP EXE
-vasp=$vasp_half
 
-working_dir=`pwd`
-vasp_files="${working_dir}/vasp_input_files"
+command_to_run='mpprun vasp > std_out_vasp.dat'
+input_files_dir='vasp_input_files/'
 
-
-# Path to where the CHGCAR, POSCAR and OUTCAR files for the self-consistent calculations are
-sc_calc_folder="${working_dir}/../step_1_get_converged_CHGCAR"
+# Path to where the CHGCAR, POSCAR and OUTCAR files for the self-cons. calculations are
+sc_calc_folder='../step_1_*/'
 CHGCAR=${sc_calc_folder}'/CHGCAR'
 POSCAR=${sc_calc_folder}'/POSCAR'
+# Grid stuff
 NGX=`grep 'dimension x,y,z NGX =' "${sc_calc_folder}/OUTCAR" | head -1 | awk '{split($0,array," ")} END{print array[5]}'`
 NGY=`grep 'dimension x,y,z NGX =' "${sc_calc_folder}/OUTCAR" | head -1 | awk '{split($0,array," ")} END{print array[8]}'`
 NGZ=`grep 'dimension x,y,z NGX =' "${sc_calc_folder}/OUTCAR" | head -1 | awk '{split($0,array," ")} END{print array[11]}'`
@@ -21,30 +15,18 @@ NGXF=`grep 'dimension x,y,z NGXF=' "${sc_calc_folder}/OUTCAR" | head -1 | awk '{
 NGYF=`grep 'dimension x,y,z NGXF=' "${sc_calc_folder}/OUTCAR" | head -1 | awk '{split($0,array," ")} END{print array[6]}'`
 NGZF=`grep 'dimension x,y,z NGXF=' "${sc_calc_folder}/OUTCAR" | head -1 | awk '{split($0,array," ")} END{print array[8]}'`
 
-for direc in "L-G_G-X_X-U_K-G"
-do
-    folder_for_current_direc="to_unfold_onto_pcbz_direc_${direc}"
-    mkdir -p ${folder_for_current_direc}
+# Preparing inputs
+cp $POSCAR POSCAR
+ln -s $CHGCAR CHGCAR
+cp $input_files_dir/POTCAR POTCAR
+cp ../step_2_*/KPOINTS_supercell.out KPOINTS
+cp $input_files_dir/INCAR INCAR
+echo "" >> INCAR
+echo " for consistency with static (total_energy) run:" >> INCAR
+echo "NGX = ${NGX}; NGY = ${NGY}; NGZ = ${NGZ}" >> INCAR
+echo "NGXF = ${NGXF}; NGYF = ${NGYF}; NGZF = ${NGZF} " >> INCAR
 
-    kpts_file_for_current_direc="${working_dir}/../step_2_get_kpts_to_be_used_in_the_SC_band_struc_calcs/KPOINTS_supercell_to_unfold_onto_pcbz_direc_${direc}.out"
-
-    cp $POSCAR ${folder_for_current_direc}/POSCAR
-    ln -s $CHGCAR ${folder_for_current_direc}/CHGCAR
-    cp $vasp_files/POTCAR ${folder_for_current_direc}/POTCAR
-    cp ${kpts_file_for_current_direc} ${folder_for_current_direc}/KPOINTS
-    cp $vasp_files/INCAR  ${folder_for_current_direc}
-
-    cd ${folder_for_current_direc}
-        echo "" >> INCAR
-        echo " for consistency with static (total_energy) run:" >> INCAR
-        echo "NGX = ${NGX}; NGY = ${NGY}; NGZ = ${NGZ}" >> INCAR
-        echo "NGXF = ${NGXF}; NGYF = ${NGYF}; NGZF = ${NGZF} " >> INCAR
-
-        mpprun  $vasp > std_out_vasp_direc_${direc}
-
-        shopt -s extglob
-        eval "rm -f !(WAVECAR|OUTCAR|POSCAR|KPOINTS|INCAR|POTCAR|std_out_vasp_direc_${direc})"
-    cd ..
-done
+# Running
+eval $command_to_run
 
 #End of script
