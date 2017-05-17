00db4b157d2bb4851872b6bb5a35ddb577660344
Paulo V C Medeiros <pvm20@cam.ac.uk>
2017-05-17 14:49:49 +0100

    Minor rearrangement

--- tutorial/VASP/example_1_graphene_rectangular_SC/step_4_run_BandUP_and_plot/run_BandUP_unfold_and_plot_tasks.sh
+++ tutorial/VASP/example_1_graphene_rectangular_SC/step_4_run_BandUP_and_plot/run_BandUP_unfold_and_plot_tasks.sh
@@ -11,6 +11,10 @@ unfolding_task_args="-emin -20 -emax 5 -dE 0.050"
 plot_task_args='-input_file unfolded_EBS_symmetry-averaged.dat --show --save'
 plot_task_args="${plot_task_args} -plotdir plot --round_cb 0"
 
+# Choose OMP_NUM_THREADS=1 if you do not want openmp parallelization. 
+# I normally use OMP_NUM_THREADS=n_cores/2
+export OMP_NUM_THREADS=2
+
 # You will probably not need to change anything below this line. I do recommend, however,
 # that you read and understand what is happening in this whole file. Once you have fully
 # understood what is done here, you'll realize that this script is not needed at all, as
@@ -30,9 +34,6 @@ task='unfold'
 task_args=${unfolding_task_args}
 command_to_run="${exe} ${task} ${task_args}"
 # Running BandUP with the task and task-specific options requested
-# Choose OMP_NUM_THREADS=1 if you do not want openmp parallelization. 
-# I normally use OMP_NUM_THREADS=n_cores/2
-export OMP_NUM_THREADS=8
 ulimit -s unlimited
 eval $command_to_run
 
