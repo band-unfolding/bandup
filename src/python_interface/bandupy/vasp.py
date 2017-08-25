3690f2b43d034929dc77d2c592cc7d271bdfbf23
Paulo V C Medeiros <pvm20@cam.ac.uk>
2017-08-25 17:31:19 +0100

    Improved routine that reads the PROCAR file, so that it can handle at least two different formats reported by BandUP users.

--- src/python_interface/bandupy/vasp.py
+++ src/python_interface/bandupy/vasp.py
@@ -20,6 +20,10 @@ import time
 # Imports from within the package
 from .constants import WORKING_DIR
 from .orbital_contributions import KptInfo
+from .warnings_wrapper import warnings, WarningError
+
+class RealAndImagPartsNotInSameLineProcar(IOError):
+    pass
 
 def procar2bandup(fpath=os.path.join(WORKING_DIR, 'PROCAR'),
                   ignore_off_diag=False, force_hermitian=False, use_dual=True):
@@ -33,16 +37,18 @@ def procar2bandup(fpath=os.path.join(WORKING_DIR, 'PROCAR'),
         only_phase=True,
     )
 
-def read_procar(fpath=os.path.join(WORKING_DIR, 'PROCAR'),
+def try_to_read_procar(fpath=os.path.join(WORKING_DIR, 'PROCAR'),
                 mode='accumulate', 
                 ignore_off_diag=False,
                 force_hermitian=False,
                 use_dual=True,
-                only_phase=False):
+                only_phase=False,
+                same_line_real_and_imag_phase=True):
 
     allowed_modes = ['accumulate', 'convert2bandup']
     if(mode not in allowed_modes):
-        raise ValueError('mode: Allowed values are %s'%(', '.join(allowed_modes)))
+        raise ValueError('mode: Allowed values are %s'%(
+                         ', '.join(allowed_modes)))
 
     # Getting orbitals present in file
     available_orbs = []
@@ -53,7 +59,7 @@ def read_procar(fpath=os.path.join(WORKING_DIR, 'PROCAR'),
                 break
     kpts_info = []
     with open(fpath, 'r') as f:
-        # Will not use readlines here because PROCAR files can often be very big
+        # Will not use readlines because PROCAR files can often be very big
         start_norm = float('Inf')
         start_phase = float('Inf')
         end_norm = float('-Inf')
@@ -69,19 +75,18 @@ def read_procar(fpath=os.path.join(WORKING_DIR, 'PROCAR'),
                 if(ispin==0 and mode=='accumulate'):
                     kpts_info = [None for ik in range(nkps)]
             elif('k-point' in line and 'weight' in line): 
-                print('Kpt #%d/%d, spin_channel=%d: Reading <atOrb|PW> projections...'%(
-                      int(lsplit[1]), nkps, ispin+1)) # TEST
-                # Some PROCAR files have a formatting problem in this line
-                new_lsplit = []
-                for i, item in enumerate(lsplit):
-                    if('-' in item and item!='k-point'):
-                        before, after = item.split('-')
-                        if(before.strip()): new_lsplit.append(before)
-                        new_lsplit.append('-%s'%(after))
-                    else:
-                        new_lsplit.append(item)
-                lsplit = new_lsplit
+                # Some PROCAR files have a formatting problem in this line,
+                # where there will be no space between two numbers if the
+                # second one has a negative sign. Accounting for this and 
+                # producing a proper lsplit for this case:
+                index_collon = line.index(":")
+                lsplit = (line[:index_collon] + 
+                          line[index_collon:].replace("-", " -")).split()
+
                 kpt_number = int(lsplit[1])
+                msg = "Kpt #%d/%d, spin_channel=%d: "%(kpt_number,nkps,ispin+1)
+                msg += "Reading <atOrb|PW> projections..."
+                print(msg)
                 if(mode=='convert2bandup' or ispin==0):
                     kpt  = KptInfo(number=kpt_number, 
                                frac_coords=map(float, lsplit[3:6]),
@@ -97,7 +102,7 @@ def read_procar(fpath=os.path.join(WORKING_DIR, 'PROCAR'),
                     kpt.nspins = 2
                 kpt.select_spin(ispin)
                 iband = 0
-            elif(("# energy" in line) and ('# occ' in line)):
+            elif(("energy" in line) and ('occ' in line)):
                 iband += 1
                 kpt.bands[iband-1]['number'] = iband
                 kpt.bands[iband-1]['ener'] = float(lsplit[4])
@@ -105,12 +110,17 @@ def read_procar(fpath=os.path.join(WORKING_DIR, 'PROCAR'),
                 start_norm = iline + 3
                 end_norm = start_norm + nions - 1
                 start_phase = end_norm + 3
-                end_phase = start_phase + 2*nions - 1
+                if(same_line_real_and_imag_phase):
+                    end_phase = start_phase + nions - 1
+                else:
+                    end_phase = start_phase + 2*nions - 1
                 iatom = 0
             elif(not only_phase and (start_norm <= iline <= end_norm+1)):
                 if(iline==end_norm+1):
                     for i, orb in enumerate(kpt.orbitals):
-                        kpt.bands[iband-1]['tot_orb_proj_norms'][orb]=float(lsplit[i+1])
+                        kpt.bands[iband-1]['tot_orb_proj_norms'][orb]= (
+                            float(lsplit[i+1])
+                        )
                 else:
                     for i, orb in enumerate(kpt.orbitals):
                         kpt.bands[iband-1]['ion_proj_norms'][iatom][orb] = (
@@ -122,18 +132,34 @@ def read_procar(fpath=os.path.join(WORKING_DIR, 'PROCAR'),
                 if(iline==start_phase): 
                     iatom = 0
                     reading_real_part = True
-                comb_iat_iorbs = [kpt.combined_atom_orb_index(iat=iatom, iorb=i) for
-                                  i in xrange(kpt.n_orbs_per_atom)]
-                if(reading_real_part):
-                    for i, comb_iat_iorb in enumerate(comb_iat_iorbs):
-                        fline_value = float(lsplit[i+1])
-                        kpt.proj_matrix[comb_iat_iorb, iband-1] = fline_value
+                comb_iat_iorbs = [kpt.combined_atom_orb_index(iat=iatom,iorb=i)
+                                  for i in xrange(kpt.n_orbs_per_atom)]
+                if(same_line_real_and_imag_phase):
+                    try:
+                        for i, comb_iat_iorb in enumerate(comb_iat_iorbs):
+                            cplx_val = (float(lsplit[i+1]) + 
+                                        1.0j * float(lsplit[i+2]))
+                            kpt.proj_matrix[comb_iat_iorb, iband-1] = cplx_val
+                        iatom += 1
+                    except(IndexError):
+                        raise(RealAndImagPartsNotInSameLineProcar)
                 else:
-                    for i, comb_iat_iorb in enumerate(comb_iat_iorbs):
-                        fline_value = float(lsplit[i+1])
-                        kpt.proj_matrix[comb_iat_iorb, iband-1] += 1.0j * fline_value
-                    iatom += 1
-                reading_real_part = not reading_real_part
+                    try:
+                        if(reading_real_part):
+                            for i, comb_iat_iorb in enumerate(comb_iat_iorbs):
+                                re_part = float(lsplit[i+1])
+                                kpt.proj_matrix[comb_iat_iorb, iband-1]=re_part
+                        else:
+                            for i, comb_iat_iorb in enumerate(comb_iat_iorbs):
+                                im_part = 1.0j * float(lsplit[i+1])
+                                kpt.proj_matrix[comb_iat_iorb,iband-1]+=im_part
+                            iatom += 1
+                        reading_real_part = not reading_real_part
+                    except(IndexError):
+                        msg = "You should use LORBIT=12 to generate your "
+                        msg += "PROCAR file. If you've done so, "
+                        msg += "plese contact us."
+                        warnings.warn(msg, category=WarningError)
                 if(iline==end_phase and iband==nbands):
                     if(mode=='convert2bandup'):
                         print('    * Calculating duals and saving results...')
@@ -146,3 +172,33 @@ def read_procar(fpath=os.path.join(WORKING_DIR, 'PROCAR'),
     elif(mode=='accumulate'):
         return kpts_info
 
+def read_procar(fpath=os.path.join(WORKING_DIR, 'PROCAR'),
+                mode='accumulate', 
+                ignore_off_diag=False,
+                force_hermitian=False,
+                use_dual=True,
+                only_phase=False):
+    try:
+        try_to_read_procar(
+            fpath=fpath,
+            mode=mode,
+            ignore_off_diag=ignore_off_diag,
+            force_hermitian=force_hermitian,
+            use_dual=use_dual,
+            only_phase=only_phase,
+            same_line_real_and_imag_phase=True
+        )
+    except(RealAndImagPartsNotInSameLineProcar):
+        msg = "    * Failed: PROCAR file format differs from the one "
+        msg += "originally assumed."
+        print(msg)
+        print("      Trying again assuming a different format:")
+        try_to_read_procar(
+            fpath=fpath,
+            mode=mode,
+            ignore_off_diag=ignore_off_diag,
+            force_hermitian=force_hermitian,
+            use_dual=use_dual,
+            only_phase=only_phase,
+            same_line_real_and_imag_phase=False
+        )
